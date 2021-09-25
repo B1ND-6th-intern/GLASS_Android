228583106e7c2f2a78bf5ac412e5fe981c9aac18
snack655 <80940200+snack655@users.noreply.github.com>
2021-09-25 17:37:10 +0900

    ADD : Login And Select Job!
    2021-09-25

--- GLASS_Android/app/src/main/java/kr/hs/dgsw/smartschool/glass_android/view/activity/LoginActivity.kt
+++ GLASS_Android/app/src/main/java/kr/hs/dgsw/smartschool/glass_android/view/activity/LoginActivity.kt
@@ -1,14 +1,13 @@
 package kr.hs.dgsw.smartschool.glass_android.view.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import kr.hs.dgsw.smartschool.glass_android.R
 import kr.hs.dgsw.smartschool.glass_android.databinding.ActivityLoginBinding
-import kr.hs.dgsw.smartschool.glass_android.databinding.ActivityMainBinding
 import kr.hs.dgsw.smartschool.glass_android.viewmodel.activity.LoginViewModel
-import kr.hs.dgsw.smartschool.glass_android.viewmodel.activity.MainViewModel
 
 class LoginActivity : AppCompatActivity() {
     lateinit var binding: ActivityLoginBinding
@@ -18,6 +17,13 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         performDataBinding()
 
+        with(loginViewModel) {
+            onLoginEvent.observe(this@LoginActivity, {
+                    val intent = Intent(this@LoginActivity, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+            })
+        }
     }
 
     private fun performDataBinding() {
--- GLASS_Android/app/src/main/java/kr/hs/dgsw/smartschool/glass_android/viewmodel/activity/LoginViewModel.kt
+++ GLASS_Android/app/src/main/java/kr/hs/dgsw/smartschool/glass_android/viewmodel/activity/LoginViewModel.kt
@@ -1,6 +1,17 @@
 package kr.hs.dgsw.smartschool.glass_android.viewmodel.activity
 
 import androidx.lifecycle.ViewModel
+import kr.hs.dgsw.smartschool.glass_android.extension.SingleLiveEvent
 
 class LoginViewModel : ViewModel() {
+    val onLoginEvent = SingleLiveEvent<Unit>()
+    val onSignUpEvent = SingleLiveEvent<Unit>()
+
+    fun onClickLogin() {
+        onLoginEvent.call()
+    }
+
+    fun onClickSignUp() {
+        onSignUpEvent.call()
+    }
 }
