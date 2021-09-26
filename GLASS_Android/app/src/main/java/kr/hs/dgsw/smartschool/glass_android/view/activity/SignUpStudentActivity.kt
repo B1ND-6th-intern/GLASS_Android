f16d28d3099ee093f0a34bf4ad76f009cc5cd33a
snack655 <80940200+snack655@users.noreply.github.com>
2021-09-26 14:15:07 +0900

    ADD : SignUp for Students UI
    2021-09-26

---
+++ GLASS_Android/app/src/main/java/kr/hs/dgsw/smartschool/glass_android/view/activity/SignUpStudentActivity.kt
@@ -0,0 +1,12 @@
+package kr.hs.dgsw.smartschool.glass_android.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kr.hs.dgsw.smartschool.glass_android.R
+
+class SignUpStudentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up_student)
+    }
+}
