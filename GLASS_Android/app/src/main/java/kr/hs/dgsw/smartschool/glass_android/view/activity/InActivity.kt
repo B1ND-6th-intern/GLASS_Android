89691f292241f4ad9421f9085a627feff144aedf
snack655 <80940200+snack655@users.noreply.github.com>
2021-10-10 03:38:08 +0900

    Add : Beutiful loading go to app
    2021-10-10
    good

---
+++ GLASS_Android/app/src/main/java/kr/hs/dgsw/smartschool/glass_android/view/activity/InActivity.kt
@@ -0,0 +1,27 @@
+package kr.hs.dgsw.smartschool.glass_android.view.activity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import kr.hs.dgsw.smartschool.glass_android.R
+
+class InActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_in)
+
+        startLoading()
+    }
+
+    private fun startLoading() {
+        val handler = Handler()
+        handler.postDelayed({
+            kotlin.run {
+                val intent = Intent(applicationContext, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }, 2000)
+    }
+}
