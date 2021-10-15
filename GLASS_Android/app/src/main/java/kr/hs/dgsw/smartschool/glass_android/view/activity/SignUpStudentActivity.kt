b8e277dedc85b9534ffc7e7609cbe649867db5fb
snack655 <80940200+snack655@users.noreply.github.com>
2021-10-15 18:01:21 +0900

    Add : Empty cell can't go
    2021-10-15

--- GLASS_Android/app/src/main/java/kr/hs/dgsw/smartschool/glass_android/view/activity/SignUpStudentActivity.kt
+++ GLASS_Android/app/src/main/java/kr/hs/dgsw/smartschool/glass_android/view/activity/SignUpStudentActivity.kt
@@ -3,15 +3,11 @@ package kr.hs.dgsw.smartschool.glass_android.view.activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.CheckBox
 import android.widget.Toast
-import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import kr.hs.dgsw.smartschool.glass_android.R
 import kr.hs.dgsw.smartschool.glass_android.databinding.ActivitySignUpStudentBinding
-import kr.hs.dgsw.smartschool.glass_android.network.response.EmailResponse
-import kr.hs.dgsw.smartschool.glass_android.viewmodel.activity.SelectJobViewModel
 import kr.hs.dgsw.smartschool.glass_android.viewmodel.activity.SignUpStudentViewModel
 
 class SignUpStudentActivity : AppCompatActivity() {
@@ -40,6 +36,10 @@ class SignUpStudentActivity : AppCompatActivity() {
 
             onBackSelectEvent.observe(this@SignUpStudentActivity, {
                 finish()
+            })
+
+            onEmptyEvent.observe(this@SignUpStudentActivity, {
+                Toast.makeText(this@SignUpStudentActivity, "칸이 비었어요!", Toast.LENGTH_SHORT).show()
             })
         }
     }
